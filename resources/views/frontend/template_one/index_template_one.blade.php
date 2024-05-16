5cb231ff2b294dc82b2664060f61d0f74dbffce7
ashiquzzaman77 <dev2.ngenit@gmail.com>
2024-05-16 13:10:31 +0600

    Some Code Push Done

--- resources/views/frontend/template_one/index_template_one.blade.php
+++ resources/views/frontend/template_one/index_template_one.blade.php
@@ -44,11 +44,6 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-<<<<<<< HEAD
-                    <p class="text-center text-white mb-0 p-3">
-                        Embark on an Epic Journey of Exploration at Dadabhaai – Where Every Click Awakens a World of Wonders and Whims!
-                    </p>
-=======
                     <div class="newsletter newsletter--box d-flex justify-content-between align-items-center pos-rel">
                         <div class="left d-flex justify-content-between align-items-center">
                             <div class="newsletter__title">
@@ -72,7 +67,6 @@
                             </button>
                         </form>
                     </div>
->>>>>>> 202353e99b5117f2cd83076ae8f750f173d07041
                 </div>
             </div>
         </div>
@@ -80,45 +74,6 @@
     <!-- Subscribe End -->
 
     <!-- modal area start --
-<<<<<<< HEAD
-              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-                  Launch demo modal
-              </button>
-
-              <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered" role="document">
-                      <div class="modal-content">
-                          <div class="modal-header">
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                              </button>
-                          </div>
-                          <div class="modal-body">
-                             <div class="modal-wrapper">
-                                 <div class="pro-img">
-                                     <img src="img/allproducts/modal-img.jpg" data-zoom-image="img/allproducts/demo.jpg" class="zoom-e-img" alt="">
-                                 </div>
-                                 <div class="pro-text">
-                                     <h4>-30% on Subscribe</h4>
-                                     <p>Five things you only know if you were at Chanel
-                                         Hamburg Show Kering Reinforces Luxury Status
-                                         By Distributing Puma.</p>
-                                      <form action="#">
-                                          <input type="email" placeholder="Enter your Email">
-                                          <button type="submit">Submit</button>
-                                          <span>
-                                              <input type="checkbox">
-                                              Prevent this pop-up
-                                          </span>
-                                      </form>
-                                 </div>
-                             </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              -- modal area end -->
-=======
           <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
               Launch demo modal
           </button>
@@ -156,5 +111,4 @@
               </div>
           </div>
           -- modal area end -->
->>>>>>> 202353e99b5117f2cd83076ae8f750f173d07041
 @endsection
