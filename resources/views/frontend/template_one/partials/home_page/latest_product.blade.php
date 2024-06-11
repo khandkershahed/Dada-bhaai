7c74df26827bfdefc5f9a87a67e22d54546f1adb
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-06-11 14:46:29 +0600

    Some Changes Done

--- resources/views/frontend/template_one/partials/home_page/latest_product.blade.php
+++ resources/views/frontend/template_one/partials/home_page/latest_product.blade.php
@@ -30,24 +30,18 @@
 
             </div>
 
-            <div class="row">
+            <div class="row gx-1">
 
                 @forelse ($products as $product)
                     <div class="col-lg-3 col-md-6">
-                        <div class="product-grid mb-3">
+                        <div class="product-grid mb-4">
                             <div class="product-image">
                                 <a href="{{ url('product' . '/' . $product->id . '/' . $product->product_slug) }}"
                                     class="image">
-
-                                    <img class="pic-1" src="{{ asset($product->product_image) }}"
-                                        data-tip="{{ $product->product_name }}" style="width:100%;height: 500px;">
-
+                                    <img src=" {{ asset($product->product_image) }}"
+                                        data-tip="{{ $product->product_name }}">
                                 </a>
-
-                                <ul class="product-labels">
-                                    <li class="px-3">New</li>
-                                </ul>
-
+                                <span class="product-discount-label">-25%</span>
                                 <ul class="product-links">
                                     <li><a style="cursor: pointer;" id="{{ $product->id }}"
                                             onclick="addToWishList(this.id)" data-tip="Wishlist"><i
@@ -55,36 +49,34 @@
                                     <li><a type="submit" style="cursor:pointer;" data-product_id="{{ $product->id }}"
                                             data-tip="Compare"><i class="fas fa-random"></i></a></li>
                                 </ul>
-                                <a type="submit" style="cursor:pointer;" data-product_id="{{ $product->id }}"
-                                    class="add-to-cart">Add to cart</a>
                             </div>
-                            <div class="product-content">
+                            <div class="product-content d-flex justify-content-between align-items-center">
                                 <div>
-                                    <span><a class="text-muted" href="#">{{ $product->brand->brand_name }}</a>
+                                    <span><a class="text-muted"
+                                            href="{{ url('product' . '/' . $product->id . '/' . $product->product_slug) }}">{{ $product->brand->brand_name }}</a>
                                     </span>
-                                    <h3 class="title"><a href="#">{{ substr($product->product_name, 0, 18) }}</a>
+                                    <h3 class="title font-weight-bold"><a
+                                            href="{{ url('product' . '/' . $product->id . '/' . $product->product_slug) }}">{{ substr($product->product_name, 0, 18) }}</a>
                                     </h3>
                                 </div>
-                                <div>
-                                    <div class="price fw-bold">
-                                        @if ($product->price_status == 'rfq')
-                                            <h6 class="grenadier-color mb-0 fw-bold">
-                                                Tk {{ $product->sas_price }}
-                                            </h6>
-                                        @elseif ($product->price_status == 'offer_price')
-                                            <del>Tk {{ $product->price }}</del>
-                                            <h6 class="grenadier-color mb-0 fw-bold">Tk {{ $product->discount_price }}
-                                            </h6>
-                                        @elseif ($product->price_status == 'price')
-                                            <h6 class="grenadier-color mb-0 fw-bold">
-                                                Tk {{ $product->price }}
-                                            </h6>
-                                        @endif
-                                    </div>
-                                    {{-- <span>
-                                        <del>250$</del>
-                                    </span> --}}
+                                <div class="price font-weight-bold pr-2">
+                                    @if ($product->price_status == 'rfq')
+                                        <h6 class="grenadier-color mb-0 font-weight-bold">
+                                            Tk {{ $product->sas_price }}
+                                        </h6>
+                                    @elseif ($product->price_status == 'offer_price')
+                                        <del>Tk {{ $product->price }}</del>
+                                        <h6 class="grenadier-color mb-0 font-weight-bold">Tk {{ $product->discount_price }}
+                                        </h6>
+                                    @elseif ($product->price_status == 'price')
+                                        <h6 class="grenadier-color mb-0 font-weight-bold">
+                                            Tk {{ $product->price }}
+                                        </h6>
+                                    @endif
                                 </div>
+                            </div>
+                            <div>
+                                <a type="submit" style="cursor:pointer;" class="add-cart" data-product_id="{{ $product->id }}">Add to cart</a>
                             </div>
                         </div>
                     </div>
