4ba05348144dab1517d22965f31f7cd2a8af50b8
ashiquzzaman77 <dev2.ngenit@gmail.com>
2024-06-25 17:02:46 +0600

    Update

--- resources/views/frontend/astell/home_page/fotter-top-slider.blade.php
+++ resources/views/frontend/astell/home_page/fotter-top-slider.blade.php
@@ -1,44 +1,28 @@
 <section class="experience">
-    <div class="swiper-container" style="margin-top:5rem; ">
+    <div class="swiper-container" style="margin-top:5rem;">
         <!-- Additional required wrapper -->
         <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                    alt="">
-            </div>
+
+            @if (
+                !empty($homepage->background_image_one_image) ||
+                    !empty($homepage->background_image_two_image) ||
+                    !empty($homepage->background_image_three_image))
+                <div class="swiper-slide">
+                    <img src="{{ asset('upload/home/' . $homepage->background_image_one_image) }}" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                    <img src="{{ asset('upload/home/' . $homepage->background_image_two_image) }}" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                    <img src="{{ asset('upload/home/' . $homepage->background_image_three_image) }}" alt="">
+                </div>
+            @endif
+
+
         </div>
+
 
         <!-- If we need navigation buttons -->
         <div class="swiper-button-prev-main"></div>
