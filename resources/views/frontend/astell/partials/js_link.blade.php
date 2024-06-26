e8ee0ac742f30948a014690e27ac4d3471b0da1d
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-06-26 09:23:49 +0600

    Some Changes Done

--- resources/views/frontend/astell/partials/js_link.blade.php
+++ resources/views/frontend/astell/partials/js_link.blade.php
@@ -32,8 +32,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
-<script src="https://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js">
-</script>
+<script src="https://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
 
 
 <script>
@@ -66,6 +65,101 @@
             }
         });
     });
+</script>
+
+<script>
+    if ($('.product__slider-main').length) {
+        var $slider = $('.product__slider-main')
+            .on('init', function(slick) {
+                $('.product__slider-main').fadeIn(1000);
+            })
+            .slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                autoplay: true,
+                lazyLoad: 'ondemand',
+                autoplaySpeed: 3000,
+                asNavFor: '.product__slider-thmb'
+            });
+
+        var $slider2 = $('.product__slider-thmb')
+            .on('init', function(slick) {
+                $('.product__slider-thmb').fadeIn(1000);
+            })
+            .slick({
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                lazyLoad: 'ondemand',
+                asNavFor: '.product__slider-main',
+                dots: false,
+                centerMode: false,
+                focusOnSelect: true
+            });
+
+        //remove active class from all thumbnail slides
+        $('.product__slider-thmb .slick-slide').removeClass('slick-active');
+
+        //set active class to first thumbnail slides
+        $('.product__slider-thmb .slick-slide').eq(0).addClass('slick-active');
+
+        // On before slide change match active thumbnail to current slide
+        $('.product__slider-main').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+            var mySlideNumber = nextSlide;
+            $('.product__slider-thmb .slick-slide').removeClass('slick-active');
+            $('.product__slider-thmb .slick-slide').eq(mySlideNumber).addClass('slick-active');
+        });
+
+
+        // init slider
+        require(['js-sliderWithProgressbar'], function(slider) {
+
+            $('.product__slider-main').each(function() {
+
+                me.slider = new slider($(this), options, sliderOptions, previewSliderOptions);
+
+                // stop slider
+                //me.slider.stop();
+
+                // start slider
+                //me.slider.start(index);
+
+                // get reference to slick slider
+                //me.slider.getSlick();
+
+            });
+        });
+        var options = {
+            progressbarSelector: '.bJS_progressbar',
+            slideSelector: '.bJS_slider',
+            previewSlideSelector: '.bJS_previewSlider',
+            progressInterval: ''
+                // add your own progressbar animation function to sync it i.e. with a video
+                // function will be called if the current preview slider item (".b_previewItem") has the data-customprogressbar="true" property set
+                ,
+            onCustomProgressbar: function($slide, $progressbar) {}
+        }
+
+        // slick slider options
+        // see: https://kenwheeler.github.io/slick/
+        var sliderOptions = {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            autoplay: true
+        }
+
+        // slick slider options
+        // see: https://kenwheeler.github.io/slick/
+        var previewSliderOptions = {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: false,
+            focusOnSelect: true,
+            centerMode: true
+        }
+    }
 </script>
 
 @stack('scripts')
