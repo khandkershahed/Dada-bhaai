c5da0c545c4252e91f0945847391c8920bbca70d
ashiquzzaman77 <dev2.ngenit@gmail.com>
2024-06-26 16:54:02 +0600

    26-06-2024 Update By Ashiquuzzaman

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if (Schema::hasTable('sites')) {
-        //     View::share('site', Sites::first());
-        // }
+        if (Schema::hasTable('sites')) {
+            View::share('site', Sites::first());
+        }
         
     }
 }
