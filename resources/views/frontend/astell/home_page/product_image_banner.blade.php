ae674b084c822241cfdb96cedec6a76b28bda9bc
shahed141123 <khandkershahed23@gmail.com>
2024-06-30 09:58:05 +0600

    single-page-shahed

--- resources/views/frontend/astell/home_page/product_image_banner.blade.php
+++ resources/views/frontend/astell/home_page/product_image_banner.blade.php
@@ -6,11 +6,7 @@
 
 <section class="brand-story scrollAni n2">
     <h2>
-<<<<<<< HEAD
-        We believe <strong>DadaBhaai</strong> is the center of Asthenia Product
-=======
         We believe <strong>DadaBhaai</strong> is the center of luxurious products.
->>>>>>> 7d9af586d18abb798ea9f6367ca7bd3bb4860b60
     </h2>
     <ul>
 
