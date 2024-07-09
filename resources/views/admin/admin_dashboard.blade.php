22413f799bfdf1d9b89674b3c594ff2dc60756ac
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-07-09 22:23:24 +0600

    nfc controller

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -6,16 +6,20 @@
 <head>
     <base href="">
 
-    <title>Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <meta charset="utf-8" />
-    <meta name="description" content="The most advanced Bootstrap " />
-    <meta name="keywords" content="Metronic, bootstrap, boo" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="description" content="{{ $site->site_slogan }}" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="article" />
-    <meta property="og:title" content="Metronic" />
-    <meta property="og:url" content="https://keenthemes.com/metronic" />
-    <meta property="og:site_name" content="Keenthemes | Metronic" />
+    <meta property="og:title" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="{{ $site->site_name }}" />
+    <title>{{ $site->site_name }}</title>
 
     {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
 
