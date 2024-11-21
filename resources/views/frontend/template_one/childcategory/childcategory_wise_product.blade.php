540b1393d8a5e277699b20f017bec385e049346a
ashiquzzaman77 <ashiquzzaman.rajib.cse@!gmail.com>
2024-11-21 17:58:33 +0600

    Update By Ashiquzzaman 16-11-2024

--- resources/views/frontend/template_one/childcategory/childcategory_wise_product.blade.php
+++ resources/views/frontend/template_one/childcategory/childcategory_wise_product.blade.php
@@ -99,7 +99,8 @@
                                                 href="{{ url('product' . '/' . $product->id . '/' . $product->product_slug) }}">{{ substr($product->product_name, 0, 25) }}</a>
                                         </h3>
                                     </div>
-                                    <div class="price font-weight-bold pr-2">
+
+                                    {{-- <div class="price font-weight-bold pr-2">
                                         @if ($product->price_status == 'rfq')
                                             <h6 class="grenadier-color mb-0 font-weight-bold">
                                                 $ {{ $product->sas_price }}
@@ -114,12 +115,36 @@
                                                 $ {{ $product->price }}
                                             </h6>
                                         @endif
+                                    </div> --}}
+
+                                    <div class="price font-weight-bold pr-2">
+                                        @if ($product->price_status == 'rfq' && !is_null($product->sas_price))
+                                            <h6 class="grenadier-color mb-0 font-weight-bold">$
+                                                {{ $product->sas_price }}</h6>
+                                        @elseif ($product->price_status == 'offer_price' && !is_null($product->price) && !is_null($product->discount_price))
+                                            <del>$ {{ $product->price }}</del>
+                                            <h6 class="grenadier-color mb-0 font-weight-bold">$
+                                                {{ $product->discount_price }}</h6>
+                                        @elseif ($product->price_status == 'price' && !is_null($product->price))
+                                            <h6 class="grenadier-color mb-0 font-weight-bold">$ {{ $product->price }}
+                                            </h6>
+                                        @endif
                                     </div>
+
                                 </div>
-                                <div>
+
+                                {{-- <div>
                                     <a type="submit" style="cursor:pointer;" class="add-cart add_to_cart_btn_product"
                                         data-product_id="{{ $product->id }}">Add to cart</a>
-                                </div>
+                                </div> --}}
+
+                                @if ($product->sas_price !== null || $product->price !== null || $product->discount_price !== null)
+                                    <a type="submit" style="cursor:pointer;" class="add-cart add_to_cart_btn_product"
+                                        data-product_id="{{ $product->id }}">Add to cart</a>
+                                @else
+                                    <a href="{{ route('template_one.contact') }}" class="add-cart">Contact Us</a>
+                                @endif
+
                             </div>
                         </div>
                     @empty
