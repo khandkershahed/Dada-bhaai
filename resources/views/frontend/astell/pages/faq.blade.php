cac3427f90d980be19a1b62c5d4148544e4a41c4
NgenItdev2 <dev2.ngenit@gmail.com>
2025-02-19 11:05:22 +0600

    Update By Ashiquzzaman 18-02-2025

--- resources/views/frontend/astell/pages/faq.blade.php
+++ resources/views/frontend/astell/pages/faq.blade.php
@@ -5,20 +5,21 @@
             <img src="{{ asset('img/pattern-background.png') }}" class="bg-img" alt="Background">
             <div class="faq-content">
                 <h1 class="faq-title">FAQs</h1>
-                <div class="faq-breadcrumbs">
+                {{-- <div class="faq-breadcrumbs">
                     <ul>
                         <li><a href="/"><i class="fa-solid fa-home text-danger"></i> Home</a></li>
                         <li><a href="/"><i class="fa-solid fa-home text-danger"></i> Two</a></li>
                         <li class="text-danger">Faqs</li>
                     </ul>
-                </div>
+                </div> --}}
             </div>
         </div>
     </section>
     <section>
         <div class="container mb-5">
             <div class="row">
-                <div class="col-lg-12">
+
+                {{-- <div class="col-lg-12">
                     <div class="text-center faq-top" style="background-image: url({{ asset(path: 'img/faqs.webp') }});">
                         <h1 class="pt-5 faq-top__title">Have Any Question?</h1>
                         <div>
@@ -29,7 +30,8 @@
                             </form>
                         </div>
                     </div>
-                </div>
+                </div> --}}
+
                 <div class="col-lg-12">
                     <div class="faq-options">
                         <div class="faq-head">
